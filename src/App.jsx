8c99a82be87c8ac08d2057fe9eb76f5763e9c0cd
ankittchoudharyy <163879006+ankittchoudharyy@users.noreply.github.com>
2025-03-05 17:48:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
 
       setTimeout(() => {
         axios({
-          url: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyC_mU22QDyEYKwlgXxEmSPCoSUCs37JR4M',// Use your actual API key here
+          url: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAoP9eQESTdI4eQzPf9m19YfSHINbOQdv8',// Use your actual API key here
           method: 'post',
           data: {
             contents: [{ parts: [{ text: question }] }],
